cdd5cb8c19faeb1617125213bc8786ff27f7ff86
graykode <nlkey2022@gmail.com>
2025-02-25 16:26:53 -0700

    feat: add mint future asset form

---
+++ components/form/future/mint-future-asset-form.stories.tsx
@@ -0,0 +1,52 @@
+import { Meta, StoryObj } from '@storybook/react'
+import '../../../styles/globals.css'
+import { zeroAddress } from 'viem'
+
+import { MintFutureAssetForm } from './mint-future-asset-form'
+
+export default {
+  title: 'Form/MintFutureAssetForm',
+  component: MintFutureAssetForm,
+  parameters: {
+    layout: 'centered',
+  },
+} as Meta<typeof MintFutureAssetForm>
+
+type Story = StoryObj<typeof MintFutureAssetForm>
+export const Default: Story = {
+  args: {
+    asset: {
+      id: '0x',
+      currency: {
+        address: zeroAddress,
+        decimals: 18,
+        name: 'AAPL',
+        symbol: 'AAPL',
+        icon: 'https://www.pyth.network/_next/image?url=%2Ficons%2Fprice-feed-icons%2Flight%2Fequity-us-aapl-usd.inline.svg&w=1920&q=75',
+      },
+      priceFeedId:
+        '0x49f6b65cb1de6b10eaf75e7c03ca029c306d0357e91b5311b175084a5ad55688',
+      collateral: {
+        address: '0x43D614B1bA4bA469fAEAa4557AEAFdec039b8795',
+        name: 'USD Coin',
+        symbol: 'USDC',
+        decimals: 6,
+      },
+      expiration: 1635724800,
+      maxLTV: 700000n,
+      liquidationThreshold: 800000n,
+      ltvPrecision: 1000000n,
+      minDebt: 10n * 10n ** 6n,
+    },
+    actionButtonProps: {
+      disabled: false,
+      onClick: () => {},
+      text: 'Borrow',
+    },
+  },
+}
+
+// @ts-ignore
+BigInt.prototype.toJSON = function () {
+  return this.toString()
+}
---
+++ components/form/future/mint-future-asset-form.tsx
@@ -0,0 +1,123 @@
+import React from 'react'
+
+import { toCommaSeparated } from '../../../utils/number'
+import { ActionButtonProps } from '../../button/action-button'
+import CurrencyAmountInput from '../../input/currency-amount-input'
+import { getLTVTextColor } from '../../../utils/ltv'
+import { Balances } from '../../../model/balances'
+import { Prices } from '../../../model/prices'
+import { Asset } from '../../../model/future/asset'
+
+export const MintFutureAssetForm = ({
+  asset,
+  maxBorrowAmount,
+  borrowLTV,
+  collateralValue,
+  setCollateralValue,
+  borrowValue,
+  setBorrowValue,
+  balances,
+  prices,
+  liquidationPrice,
+  actionButtonProps,
+  children,
+}: {
+  asset: Asset
+  maxBorrowAmount: bigint
+  borrowLTV: number
+  collateralValue: string
+  setCollateralValue: (value: string) => void
+  borrowValue: string
+  setBorrowValue: (value: string) => void
+  balances: Balances
+  prices: Prices
+  liquidationPrice: number
+  actionButtonProps: ActionButtonProps
+} & React.PropsWithChildren) => {
+  console.log('MintFutureAssetForm', actionButtonProps)
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 bg-[#171b24] rounded-xl sm:rounded-3xl p-4 sm:p-6 sm:pb-8 w-full sm:w-[480px]">
+        <div className="flex flex-col gap-4">
+          <div className="font-bold text-sm sm:text-lg">
+            How much collateral would you like to add?
+          </div>
+          <CurrencyAmountInput
+            currency={asset.collateral}
+            value={collateralValue}
+            onValueChange={setCollateralValue}
+            availableAmount={balances[asset.collateral.address] ?? 0n}
+            price={prices[asset.collateral.address]}
+          />
+          <div className="flex ml-auto">{children}</div>
+        </div>
+
+        <div className="flex flex-col gap-4">
+          <div className="font-bold text-sm sm:text-lg">
+            How much would you like to borrow?
+          </div>
+          <CurrencyAmountInput
+            currency={asset.currency}
+            value={borrowValue}
+            onValueChange={setBorrowValue}
+            price={prices[asset.currency.address]}
+            availableAmount={maxBorrowAmount}
+          />
+        </div>
+      </div>
+      <div className="flex flex-col gap-6 bg-[#171b24] rounded-xl sm:rounded-3xl p-4 sm:p-6 w-full sm:w-[480px]">
+        <div className="flex flex-col items-start gap-6 self-stretch">
+          <div className="font-bold text-sm sm:text-lg">
+            Transaction Overview
+          </div>
+          <div className="flex flex-col items-start gap-3 self-stretch">
+            <div className="flex w-full">
+              <div className="text-gray-400 text-sm sm:text-base">LTV</div>
+              <div
+                className={`ml-auto text-sm sm:text-base ${getLTVTextColor(borrowLTV, asset)}`}
+              >
+                {borrowLTV.toFixed(2)}%
+              </div>
+            </div>
+            <div className="flex w-full">
+              <div className="text-gray-400 text-sm sm:text-base">
+                Liquidation LTV
+              </div>
+              <div className="ml-auto text-sm sm:text-base">
+                {Number(
+                  (asset.liquidationThreshold * 100n) / asset.ltvPrecision,
+                ).toFixed(2)}
+                %
+              </div>
+            </div>
+            <div className="flex w-full">
+              <div className="text-gray-400 text-sm sm:text-base">
+                Liquidation Price
+              </div>
+              <div className="ml-auto text-sm sm:text-base">
+                {liquidationPrice ? (
+                  <>${toCommaSeparated(liquidationPrice.toFixed(2))}</>
+                ) : (
+                  <>-</>
+                )}
+              </div>
+            </div>
+            <div className="flex w-full">
+              <div className="flex flex-row items-center justify-center gap-1 text-gray-400 text-sm sm:text-base">
+                Expired Date
+              </div>
+              <div className="ml-auto text-sm sm:text-base">
+                {new Date(asset.expiration * 1000).toLocaleDateString('en-US', {
+                  year: 'numeric',
+                  month: 'short',
+                  day: '2-digit',
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
+        {/*<ActionButton {...actionButtonProps} />*/}
+      </div>
+    </div>
+  )
+}
