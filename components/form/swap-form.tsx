ae2943ae3247bba23d3bbe92ccf55bd640908829
graykode <nlkey2022@gmail.com>
2025-03-10 15:32:06 +0900

    feat: remove circle svg

--- components/form/swap-form.tsx
+++ components/form/swap-form.tsx
@@ -264,15 +264,6 @@ export const SwapForm = ({
                         {aggregatorName.length > 0 ? (
                           <>
                             via
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width="16"
-                              height="16"
-                              viewBox="0 0 16 16"
-                              fill="none"
-                            >
-                              <circle cx="8" cy="8" r="8" fill="#D9D9D9" />
-                            </svg>
                             <div className="text-gray-400">
                               {aggregatorName}
                             </div>
