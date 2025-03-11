50f4ec3cd76c22584da07a99f3488c95ff1e180a
graykode <nlkey2022@gmail.com>
2025-03-11 11:09:45 +0900

    fix: change base rpc

--- constants/rpc-urls.ts
+++ constants/rpc-urls.ts
@@ -3,8 +3,7 @@ import { CHAIN_IDS } from '@clober/v2-sdk'
 export const RPC_URL: {
   [chain in number]: string
 } = {
-  [CHAIN_IDS.BASE]:
-    'https://base-mainnet.g.alchemy.com/v2/3Um4IcT1mrq2MEOYurXvsRAzk_v3Q_4X',
+  [CHAIN_IDS.BASE]: 'https://mainnet.base.org',
   [CHAIN_IDS.MONAD_TESTNET]:
     'https://side-cold-water.monad-testnet.quiknode.pro/cdca51dfe940664aca31fad3acaee682eee43c3b/',
 }
