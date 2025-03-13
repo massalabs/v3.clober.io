0de951ec2b58f0f82fce9a3987421fbd8296c990
graykode <nlkey2022@gmail.com>
2025-03-13 12:17:27 +0900

    feat: update rpc url

--- constants/rpc-urls.ts
+++ constants/rpc-urls.ts
@@ -3,7 +3,8 @@ import { CHAIN_IDS } from '@clober/v2-sdk'
 export const RPC_URL: {
   [chain in number]: string
 } = {
-  [CHAIN_IDS.BASE]: 'https://mainnet.base.org',
+  [CHAIN_IDS.BASE]:
+    'https://base.blockpi.network/v1/rpc/d19f249ccf70c63f55e6fcddf5903edfc7b8df32',
   [CHAIN_IDS.MONAD_TESTNET]:
     'https://monad-testnet.g.alchemy.com/v2/YLpSdYvLaNm8IvQ48KISOiCfNfR163tJ',
 }
