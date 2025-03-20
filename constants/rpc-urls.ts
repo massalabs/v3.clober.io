f3f18f80eccdfee197bae111016af467aeaa9018
graykode <nlkey2022@gmail.com>
2025-03-20 13:09:31 +0900

    feat: use blockpi rpc

--- constants/rpc-urls.ts
+++ constants/rpc-urls.ts
@@ -6,5 +6,5 @@ export const RPC_URL: {
   [CHAIN_IDS.BASE]:
     'https://base.blockpi.network/v1/rpc/d19f249ccf70c63f55e6fcddf5903edfc7b8df32',
   [CHAIN_IDS.MONAD_TESTNET]:
-    'https://monad-testnet.blockvision.org/v1/2uIMqtxI4tKB9wkTX3ozeutNKfp',
+    'https://monad-testnet.blockpi.network/v1/rpc/48b2f88c10bee99c50afa3dc06a5d74e85311638',
 }
