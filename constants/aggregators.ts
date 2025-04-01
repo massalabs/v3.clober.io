c0a50828e69cfef512212733b4bb1c4cb5d73dff
graykode <nlkey2022@gmail.com>
2025-04-02 01:03:47 +0900

    feat: remove odos swap

--- constants/aggregators.ts
+++ constants/aggregators.ts
@@ -2,7 +2,6 @@ import { CHAIN_IDS, getContractAddresses } from '@clober/v2-sdk'
 import { getAddress } from 'viem'
 
 import { Aggregator } from '../model/aggregator'
-import { OdosAggregator } from '../model/aggregator/odos'
 import { MagpieAggregator } from '../model/aggregator/magpie'
 import { OpenOceanAggregator } from '../model/aggregator/openocean'
 import { CloberV2Aggregator } from '../model/aggregator/clober-v2'
@@ -13,10 +12,10 @@ export const AGGREGATORS: {
   [chain in CHAIN_IDS]: Aggregator[]
 } = {
   [CHAIN_IDS.BASE]: [
-    new OdosAggregator(
-      getAddress('0x19cEeAd7105607Cd444F5ad10dd51356436095a1'),
-      findSupportChain(CHAIN_IDS.BASE.valueOf())!,
-    ),
+    // new OdosAggregator(
+    //   getAddress('0x19cEeAd7105607Cd444F5ad10dd51356436095a1'),
+    //   findSupportChain(CHAIN_IDS.BASE.valueOf())!,
+    // ),
     new MagpieAggregator(
       getAddress('0xef42f78d25f4c681dcad2597fa04877ff802ef4b'),
       findSupportChain(CHAIN_IDS.BASE.valueOf())!,
