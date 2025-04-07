25b56129295f4730cb2611b36e657d39cecef517
graykode <nlkey2022@gmail.com>
2025-04-07 15:19:26 +0900

    feat: change to blockpi

--- constants/rpc-url.ts
+++ constants/rpc-url.ts
@@ -6,5 +6,5 @@ export const RPC_URL: {
   [CHAIN_IDS.BASE]:
     'https://base-mainnet.g.alchemy.com/v2/3Um4IcT1mrq2MEOYurXvsRAzk_v3Q_4X',
   [CHAIN_IDS.MONAD_TESTNET]:
-    'https://monad-testnet.blockvision.org/v1/2uIMqtxI4tKB9wkTX3ozeutNKfp',
+    'https://monad-testnet.blockpi.network/v1/rpc/f58e2c25e2e1239627d95af2fbaf1130d123c0a3',
 }
