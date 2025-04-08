f457f827ae1d28518dd0c22930273c853a59a15b
graykode <nlkey2022@gmail.com>
2025-04-08 14:27:53 +0900

    refactor: remove unused import

--- components/form/swap-form.tsx
+++ components/form/swap-form.tsx
@@ -1,7 +1,6 @@
 import React, { useCallback, useMemo } from 'react'
 import { isAddressEqual, parseUnits } from 'viem'
 import { CHAIN_IDS } from '@clober/v2-sdk'
-import BigNumber from 'bignumber.js'
 
 import CurrencyAmountInput from '../input/currency-amount-input'
 import { Currency } from '../../model/currency'
