bd6380ce48f5348566318d3c05c84c9bb7dfd608
graykode <nlkey2022@gmail.com>
2025-04-11 16:50:32 +0900

    docs: add comment for transaction types

--- model/transaction-type.ts
+++ model/transaction-type.ts
@@ -1,20 +1,20 @@
 export type TransactionType =
-  | 'approve'
-  | 'mint'
-  | 'burn'
-  | 'register'
-  | 'borrow'
-  | 'repay'
-  | 'repay-all'
-  | 'settle'
-  | 'close'
-  | 'redeem'
-  | 'add-collateral'
-  | 'remove-collateral'
-  | 'make'
-  | 'take'
-  | 'limit'
-  | 'cancel'
-  | 'claim'
-  | 'market'
-  | 'swap'
+  | 'approve' // approve erc20 token
+  | 'mint' // add liquidity into the vault
+  | 'burn' // remove liquidity from the vault
+  | 'register' // register for trading competition
+  | 'borrow' // borrow a futures asset
+  | 'repay' // repay a futures asset
+  | 'repay-all' // repay all a futures asset
+  | 'settle' // settle a futures asset
+  | 'close' // close a futures asset
+  | 'redeem' // redeem a futures asset
+  | 'add-collateral' // add collateral to a futures asset
+  | 'remove-collateral' // remove collateral from a futures asset
+  | 'make' // make a limit order
+  | 'take' // take a limit order
+  | 'limit' // limit order
+  | 'cancel' // cancel limit order(s)
+  | 'claim' // claim limit order(s)
+  | 'market' // market order
+  | 'swap' // swap from aggregator
