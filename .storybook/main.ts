df6b0f2214ea33fc782c3d51dde0ea2b6f47581b
graykode <nlkey2022@gmail.com>
2025-04-15 15:54:25 +0900

    feat: upgrade storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,27 @@
 import type { StorybookConfig } from '@storybook/nextjs'
 
+// @ts-ignore
 const config: StorybookConfig = {
   stories: ['../components/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
   ],
+
   framework: {
     name: '@storybook/nextjs',
     options: {},
   },
+
   docs: {
     autodocs: 'tag',
   },
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript'
+  }
 }
 export default config
