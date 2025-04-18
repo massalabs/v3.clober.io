bd842873a92d7146de2fc1219385260ca45e0e7d
graykode <nlkey2022@gmail.com>
2025-04-18 12:31:47 +0900

    feat: gmond price

--- model/aggregator/clober-v2.ts
+++ model/aggregator/clober-v2.ts
@@ -47,28 +47,32 @@ export class CloberV2Aggregator implements Aggregator {
   public async prices(): Promise<Prices> {
     if (this.chain.id === monadTestnet.id) {
       const {
-        data: { monChartLogs, muBondChartLogs, aprMonChartLogs },
+        data: { monChartLogs, muBondChartLogs, aprMonChartLogs, gMonChartLogs },
       } = await Subgraph.get<{
         data: {
           monChartLogs: { id: string; close: string }[]
           muBondChartLogs: { id: string; close: string }[]
           aprMonChartLogs: { id: string; close: string }[]
+          gMonChartLogs: { id: string; close: string }[]
         }
       }>(
         getSubgraphEndpoint({ chainId: this.chain.id }),
         '',
-        '{ monChartLogs: chartLogs( first: 1 orderBy: timestamp orderDirection: desc where: {marketCode: "0x0000000000000000000000000000000000000000/0xf817257fed379853cde0fa4f97ab987181b1e5ea"} ) { id close } muBondChartLogs: chartLogs( first: 1 orderBy: timestamp orderDirection: desc where: {marketCode: "0x0efed4d9fb7863ccc7bb392847c08dcd00fe9be2/0xf817257fed379853cde0fa4f97ab987181b1e5ea"} ) { id close } aprMonChartLogs: chartLogs( first: 1 orderBy: timestamp orderDirection: desc where: {marketCode: "0xb2f82d0f38dc453d596ad40a37799446cc89274a/0x0000000000000000000000000000000000000000"} ) { id close } }',
+        '{ monChartLogs: chartLogs( first: 1 orderBy: timestamp orderDirection: desc where: {marketCode: "0x0000000000000000000000000000000000000000/0xf817257fed379853cde0fa4f97ab987181b1e5ea"} ) { id close } muBondChartLogs: chartLogs( first: 1 orderBy: timestamp orderDirection: desc where: {marketCode: "0x0efed4d9fb7863ccc7bb392847c08dcd00fe9be2/0xf817257fed379853cde0fa4f97ab987181b1e5ea"} ) { id close } aprMonChartLogs: chartLogs( first: 1 orderBy: timestamp orderDirection: desc where: {marketCode: "0xb2f82d0f38dc453d596ad40a37799446cc89274a/0x0000000000000000000000000000000000000000"} ) { id close } gMonChartLogs: chartLogs( first: 1 orderBy: timestamp orderDirection: desc where: {marketCode: "0xaeef2f6b429cb59c9b2d7bb2141ada993e8571c3/0x0000000000000000000000000000000000000000"} ) { id close } }',
         {},
       )
       const monPrice = Number(monChartLogs?.[0]?.close ?? 0)
       const muBondPrice = Number(muBondChartLogs?.[0]?.close ?? 0)
       const aprMonPrice = Number(aprMonChartLogs?.[0]?.close ?? 0)
+      const gMonPrice = Number(gMonChartLogs?.[0]?.close ?? 0)
       return {
         [zeroAddress]: monPrice,
         [getAddress('0x760AfE86e5de5fa0Ee542fc7B7B713e1c5425701')]: monPrice,
         [getAddress('0x0EfeD4D9fB7863ccC7bb392847C08dCd00FE9bE2')]: muBondPrice,
         [getAddress('0xb2f82D0f38dc453D596Ad40A37799446Cc89274A')]:
           monPrice * aprMonPrice,
+        [getAddress('0xaEef2f6B429Cb59C9B2D7bB2141ADa993E8571c3')]:
+          monPrice * gMonPrice,
       }
     }
     return {} as Prices
